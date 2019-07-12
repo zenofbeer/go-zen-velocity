4c725966e7798c6dae5eed6e47f1d0657cfd3aa5
Paul Christensen <p.ellischristensen@gmail.com>
2019-07-11 21:50:12 -0700

    create some basic routing and UI. We'll build out from here.

--- main.go
+++ main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	// declare a new router
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", handler).Methods("GET")
-
-	http.ListenAndServe(":8080", nil)
+// Todo ...
+type Todo struct {
+	Title string
+	Done  bool
 }
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	// pipe "Hello World" into the response writer
-	fmt.Fprintf(writer, "Hello World!")
+// TodoPageData ...
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	r := newRouter()
+
+	http.ListenAndServe(":8080", r)
+}
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/velocity", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./resources/")
+	staticFileHandler := http.StripPrefix("/resources/", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/resources/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./resources/layout.html"))
+	data := TodoPageData{
+		PageTitle: "My ToDo list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
 }
